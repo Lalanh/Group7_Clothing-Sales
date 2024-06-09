70a91e1922490ab3a193f7a8507b7358d0c46814
Lalanh <145445716+Lalanh@users.noreply.github.com>
2024-06-10 02:25:21 +0700

    Add files via upload

---
+++ Codes website/Sales.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+
+def app():
+       
+    st.title('Sales prediction ' )
+
+    st.subheader('The sales volume of Skirt', )
+    st.image("skirt.jfif")
+    st.subheader('The sales volume of Dress', )
+    st.image("dress.jfif")
+    st.subheader('The sales volume of Shirt', )
+    st.image("shirt.jfif")
+    st.subheader('The sales volume of Blouse', )
+    st.image("blouse.jfif")
+
+
+
+    
